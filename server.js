1a70c17ad40d76672ddc1039c1253b1efc86c3be
itscottpool <it.scottpool@gmail.com>
2019-07-17 00:34:18 -0400

    updating database URL

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'kiros',
-        password: 'K!rstenlove050707',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
